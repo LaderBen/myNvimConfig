e35d4c5b66aad43b90bccf44f7389a57c93879bb
LaderBen <z874154748@sina.com>
2022-12-05 22:00:56 -0500

    update vim-airline configration

--- lua/user/vim-airline.lua
+++ lua/user/vim-airline.lua
@@ -1,8 +1,20 @@
 vim.cmd([[
+let g:airline_powerline_fonts = 1
+
+if !exists('g:airline_symbols')
+    let g:airline_symbols = {}
+endif
+
 let g:airline#extensions#tabline#enabled = 1
 let g:airline#extensions#tabline#left_sep = ' '
 let g:airline#extensions#tabline#left_alt_sep = '|'
 let g:airline#extensions#tabline#formatter = 'default'
 let g:airline_theme='powerlineish'
-let g:airline_left_sep = '▶'
+let g:airline_left_sep = ''
+let g:airline_left_alt_sep = ''
+let g:airline_right_sep = ''
+let g:airline_right_alt_sep = ''
+let g:airline_symbols.branch = ''
+let g:airline_symbols.readonly = ''
+let g:airline_symbols.linenr = ''
 ]])
